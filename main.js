155e6ec11bac43be909f443c23a1a08e0e3dee77
jo <johannes_kaendler@web.de>
2023-02-21 20:42:33 +0000

    added message box

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-function sendMessage(message) {
-    fetch("https://gotify.jokaendler.eu/message", {
-        method: "post",
-        headers: {
-            'X-Gotify-Key': 'A06KxOiecnECwpA',
-        },
-
-        //make sure to serialize your JSON body
-        body: JSON.stringify({
-            "message": message,
-            "priority": 2,
-            "title": "Message"
+function sendMessage() {
+    textarea = document.getElementById('textarea') ;
+    message = textarea.value
+    if (message != ""){
+        fetch("https://mailbox.jokaendler.eu/", {
+            method: "post",
+            body: JSON.stringify({
+                "message": textarea.value,
+                "source": "boredJo.eu"
+            })
+        }).then(response => {
+            console.log(response)
+            textarea.value = ""
         })
-    })
+
+    }
+}
+
+function toggleMessageBox() {   
+    messageBox = document.querySelector('.message-box');
+    messageBox.style.display = (messageBox.style.display === 'none') ? 'block' :'none';
 }
